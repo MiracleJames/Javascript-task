f3464d1a632904d9e29f02c67f7f6a7f3b6a5b61
MiracleJames <olotuchejames@gmail.com>
2022-06-24 11:47:56 -0700

    First commit

---
+++ life.js
@@ -0,0 +1,16 @@
+let myName = 'Miracle James Olotuche';
+let myHeight= '5 feet, 6 inches';
+let myCountry= 'Nigeria';
+
+console.log(myName);
+console.log(myHeight);
+console.log(myCountry);
+
+alert('Miracle James Olotuche');
+alert('5 feet, 6inches');
+alert('Nigeria');
+
+
+document.write("Miracle James Olotuche.<br/>");
+document.write("5 feet, 6inches.<br/>");
+document.write("Nigeria.<br/>");
